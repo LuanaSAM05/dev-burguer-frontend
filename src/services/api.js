9eaa85f84ec42042d9fff11976b3b6b6d82f4a67
Luana Melo <lsilvaalvesmelo@gmail.com>
2026-04-30 12:31:11 +0100

    fix: remove env from git

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(config => {
-    const userData = localStorage.getItem('devburguer:userData');
+  const userData = localStorage.getItem("devburguer:userData");
 
-    const token = userData && JSON.parse(userData).token;
-   
-       config.headers.authorization = `Bearer ${token}`;
+  const token = userData && JSON.parse(userData).token;
 
-    return config;
-    });
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
