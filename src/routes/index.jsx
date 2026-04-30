175351af486adf3e564ed9e57276a3e33758640e
Luana Melo <lsilvaalvesmelo@gmail.com>
2026-04-30 12:41:28 +0100

    deploy frontend

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,52 +1,43 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 import {
-    Cart,
-    Checkout,
-    CompletePayment,
-    Home,
-    Login,
-    Menu,
-    Register
-} from '../containers';
+  Cart,
+  Checkout,
+  CompletePayment,
+  Home,
+  Login,
+  Menu,
+  Register
+} from "../containers";
 
-import { UserLayout } from '../layouts/UserLayout';
-import { AdminLayout } from '../layouts/AdminLayout';
+import { UserLayout } from "../layouts/UserLayout";
+import { AdminLayout } from "../layouts/AdminLayout";
 
-import { Orders } from '../containers/Admin/Orders';
-import { NewProduct } from '../containers/Admin/NewProduct';
-import { EditProduct } from '../containers/Admin/EditProduct';
-import { Products } from '../containers/Admin/Products';
+import { Orders } from "../containers/Admin/Orders";
+import { NewProduct } from "../containers/Admin/NewProduct";
+import { EditProduct } from "../containers/Admin/EditProduct";
+import { Products } from "../containers/Admin/Products";
 
 export function Router() {
-    return (
-        <Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route index element={<Home />} />
+        <Route path="cardapio" element={<Menu />} />
+        <Route path="carrinho" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="complete-payment" element={<CompletePayment />} />
+      </Route>
 
-            {/* 🔵 ROTAS DO USUÁRIO */}
-            <Route path="/" element={<UserLayout />}>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="pedidos" element={<Orders />} />
+        <Route path="novo-produto" element={<NewProduct />} />
+        <Route path="editar-produto" element={<EditProduct />} />
+        <Route path="produtos" element={<Products />} />
+      </Route>
 
-                {/* 🔥 HOME como index (CORRETO NO V6) */}
-                <Route index element={<Home />} />
-
-                <Route path="cardapio" element={<Menu />} />
-                <Route path="carrinho" element={<Cart />} />
-                <Route path="checkout" element={<Checkout />} />
-                <Route path="complete-payment" element={<CompletePayment />} />
-
-            </Route>
-
-            {/* 🔵 ADMIN */}
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route path="pedidos" element={<Orders />} />
-                <Route path="novo-produto" element={<NewProduct />} />
-                <Route path="editar-produto" element={<EditProduct />} />
-                <Route path="produtos" element={<Products />} />
-            </Route>
-
-            {/* 🔵 AUTH */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Register />} />
-
-        </Routes>
-    );
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  );
 }
